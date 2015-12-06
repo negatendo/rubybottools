706cc83ee0e6f20e1355399ca300768b18be87ce
Brett O'Connor <brett@blimpsgo90.com>
2015-12-06 07:43:21 -0700

    corpora thing working

--- corporagulp.rb
+++ corporagulp.rb
@@ -1,25 +1,60 @@
 # encoding: utf-8
 #!/usr/bin/ruby
 
+#TODO: make me work on json files with more than one property
+
 require 'linguistics'
+require 'json'
 
 class CorporaGulp
   #simple corpora json iterator with pluralizer and maybe other things
-  def initalize
-    @path = '~/Documents/corpora/data/animals/common.json'
-    @pluralize = true
-    #TODO set last word if bot crashes
-    #@last_word = 'mice'
-    @data = self.load_corpora
+
+  attr_reader :data #data will contain the ... data
+
+  Linguistics.use( :en )
+
+  def initialize(path,resource)
+    #corpora dir + rest of path to json
+    @full_path = path + "data" + resource + ".json"
+    #read in raw data
+    @data = JSON.parse(File.read(@full_path))
+    @iterators = {} #shifted arrays of each iterated property
   end
 
-  def load_corpora
-    return JSON
+  def get_next( property, pluralize )
+    #see if already tracking this property
+    if !@iterators.include? property
+      @iterators[property] = @data[ property ]
+    end
+    return @iterators["vegetables"].shift
   end
 
-  def next_word
-    #get the next word from the list
-    #pluralize if requested
+  def get_all(property,pluralize)
+  end
+
+  def get_random( property, pluralize )
+    #random specified property from data
+    sample = @data[property].sample
+    if pluralize
+      return sample.en.plural
+    else
+      return sample
+    end
   end
 
 end
+
+
+#vegetables = CorporaGulp.new('/home/bretto/Documents/corpora/','/foods/vegetables')
+#puts vegetables.data
+#i = 0
+#while i < 100 do
+#  i += 1
+#  veg = vegetables.get_next('vegetables',true)
+#  if veg
+#    puts veg
+#  else
+#    exit
+#  end
+#end
+#puts vegetables.get_random('vegetables',true)
