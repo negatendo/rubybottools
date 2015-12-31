6c4fe337d3129da8635eb949025c3b9c9852028a
Brett O'Connor <brett@blimpsgo90.com>
2015-12-31 08:39:09 -0700

    updated

--- followerthing.rb
+++ followerthing.rb
@@ -8,48 +8,82 @@ require 'twitter'
 # the delay between all actions performed on api
 # we are nice to twitter. so nice. that's why this
 # follower thing works, you see.
-$delay_in_sec = 5
+$twitter_client_delay = 5
 
 class FollowerThing
   attr_accessor :client
 
+  attr_reader :follow_parity_running
+
   def initialize(client, username = nil)
     # FEED ME TWITTER CLIENT and optional username to eat
+    @client = client
     if !username
       username = client.user.screen_name
     end
+    @delay = $twitter_client_delay
+
     # take back the user
     @username = username
-
-    @
   end
 
   def follow_parity
-    twitter.following.each do |x|
-    sleep(3) #be nice to api
-    if !x.following?
-    twitter.unfollow(x)
-    log "Unfollowed #{x.screen_name}"
+
+    if @last_command_running
+      raise "A FollowerThing command is already running. Try again later."
     end
-    end
-    twitter.following.each do |x|
-    sleep(3) #be nice to api
-    if x.following?
-    twitter.follow(x)
-    log "Followed #{x.screen_name}"
-    end
-    end
+
+    @last_command_running = true
+    self.unfollow_non_followers
+    self.followback
+    @last_command_running = false
+  end
+
+  def unfollow_non_followers
+    @client.following.each do |x|
+      if !x.following?
+        @client.unfollow(x)
+        puts "Unfollowed #{x.screen_name}"
+      end
+      sleep(@delay)
     end
   end
 
-def
+  def followback
+    @client.followers.each do |x|
+      if x.following?
+        @client.follow(x)
+        puts "Followed #{x.screen_name}"
+      end
+      sleep(@delay)
+    end
+  end
 
+  def unfollow_all
+    @client.following.each do |x|
+      @client.unfollow(x)
+      puts "Unfollowed #{x.screen_name}"
+      sleep(@delay)
+    end
+  end
+
+  def follow_array(usernames = Array.new())
+    usernames.each do |x|
+      @client.follow(x)
+      puts "Followed #{x}"
+      sleep(@delay)
+    end
+  end
+
+end
 
 # YUMMY EXAMPLE
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "lN1fHeFIm7LTAKQYV03DDpVNO"
-  config.consumer_secret     = "2DviCQ3dDXBBU4csQcbHxcmJwQaoLYCxXDdcGaY7mpCgGabw9a"
-  config.access_token        = "2227152482-wJCVN8e0144H1QMLaDAzVVbK8oIeNjbivIg5UXQ"
-  config.access_token_secret = "hRJ4xN5wGXGUhMfA3jYh7iwX3quwxr9sX4osUVjTsUdoY"
-end
-x = FollowerThing.new(client,'negatendo')
+# Protip: use Thread class to multithread
+#client = Twitter::REST::Client.new do |config|
+#  config.consumer_key        = ""
+#  config.consumer_secret     = ""
+#  config.access_token        = ""
+#  config.access_token_secret = ""
+#end
+#x = FollowerThing.new(client,'negatendo')
+#x.follow_array(['twitter','logoninternet'])
