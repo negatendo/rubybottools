26f9ffe7d736e7ab997af9d3de52cfba2f03f2c7
Brett O'Connor <brett@blimpsgo90.com>
2016-01-11 16:37:26 -0700

    actually works

--- twittersession.rb
+++ twittersession.rb
@@ -1,26 +1,97 @@
 #!/usr/bin/ruby
 
-require 'psstore'
+require 'pstore'
 
 class TwitterSession
-  # thingy to track persistant data about interaction sessions
-  # with other users
+  # thingy to track persistant data about bot twitter interactions
 
-  attr_reader :data #data will contain the ... data
+  attr_reader :users #user interaction tracker stuffs
+  attr_accessor :data #use this to store any old thing
 
-  def initialize
-    @data = {}
+  def initialize(name, path = '')
+    @store = PStore.new(path + name + '.session.pstore')
+    @data = @store.transaction { @store['data'] }
+    @users = @store.transaction { @store['users'] }
   end
 
-  def add ( username, hash )
+  def save_data(data)
+    # er couldn't figure out how to not make a seperate "set"
+    # for transactions? general session data
+    self.save('data',data)
+  end
+
+  def data
+    @data = @store.transaction { @store['data'] }
+    return @data
+  end
+
+  def log_user_interaction( username )
+    user = self.get_user( username )
+    user.log_time()
+    self.save('users',user.get_data_array())
+  end
+
+  def save_user_data( username, data )
+    user = self.get_user( username )
+    user.data = data
+    self.save('users',user.get_data_array())
+  end
+
+  def get_user( username )
+    @users = @store.transaction { @store['users'] }
+    if @users
+      this_user = @users[username]
+      if this_user
+        return TwitterSessionUser.new( username, this_user )
+      end
+    end
+    # return new user obj if nothing
+    return TwitterSessionUser.new( username )
+  end
+
+  def save(part,data)
+    @store.transaction do
+      @store[part] = data
+      @store.commit
+    end
   end
 
 end
 
+class TwitterSessionUser
+  # seperate user class
+  attr_reader :username
+  attr_accessor :last_interaction_time, :data
 
-# session = TwitterSession.new
-# session.add('negatendo',{:poop => "things"})
-# ...
-# data = TwitterSession.lookup('negatendo')
-# puts data
-# session.clear('negatendo')
+  def initialize( username, existing_data = false )
+    @username = username
+    if existing_data
+      @data = existing_data['data']
+      @last_interaction_time = existing_data['last_interaction_time']
+    else
+      @data = { }
+      @last_interaction_time = nil #that way we know no interactions
+    end
+  end
+
+  def log_time
+    @last_interaction_time = Time.now.getutc
+  end
+
+  def get_data_array
+    # returns array ready for our pstore bits
+    return { @username => { 'last_interaction_time' => @last_interaction_time, 'data' => @data } }
+  end
+
+end
+
+# EXAMPLE
+#session = TwitterSession.new(name = 'botname', path = '/home/bretto/')
+#session.save_data({'poop' => 'shit'})
+#puts session.data['poop']
+#session.log_user_interaction('logoninternet')
+#puts session.get_user('logoninternet').last_interaction_time
+#data = { 'butt' => 'nozzles' }
+#session.save_user_data('logoninternet',data)
+#puts session.get_user('logoninternet').data
+
