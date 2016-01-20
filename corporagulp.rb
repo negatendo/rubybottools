dee2133c72521f41bf05866a75a6f8e84cf3d3ce
Brett O'Connor <brett@blimpsgo90.com>
2016-01-20 02:01:44 +0000

    simpliiifieiedd

--- corporagulp.rb
+++ corporagulp.rb
@@ -7,25 +7,18 @@ class CorporaGulp
   # thing to navigate and assemble corpora data
   # see: https://github.com/dariusk/corpora
 
-  Linguistics.use( :en )
-
   attr_reader :data_dir, :data
 
-  def initialize
+  def initialize( folder, filename, property )
     # make sure we have our data directory availabe
     if !File.directory? (File.dirname(__FILE__) + '/corpora')
       puts "Corpora not found. Did you init submodules?"
       exit
     else
       @data_dir = File.dirname(__FILE__) + '/corpora/data/'
+      file = JSON.parse(File.read(@data_dir + folder + '/' + filename))
+      @data = file[property]
     end
-  end
-
-  def load( folder, filename, property )
-    # loads into this object's data array the
-    # property data specified in the folder and filename.json
-    file = JSON.parse(File.read(@data_dir + folder + '/' + filename))
-    @data = file[property]
   end
 
   def get_all
@@ -39,6 +32,5 @@ class CorporaGulp
 end
 
 
-#x = CorporaGulp.new
-#x.load('animals','dogs.json','dogs')
+#x = CorporaGulp.new('animals','dogs.json','dogs')
 #puts x.get_random
